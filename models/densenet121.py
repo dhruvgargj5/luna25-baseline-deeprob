266c956f0f594b73769dcaf2d7eed78baf27a3be
Anant Garg <anantgar@umich.edu>
2025-04-11 23:15:31 -0400

    add Bayesian Linear

--- models/densenet121.py
+++ models/densenet121.py
@@ -1,18 +1,18 @@
 import torch
 from torch import nn
 from torchvision import models
-from torchinfo import summary
-
+from blitz.modules import BayesianLinear
+# from torchinfo import summary
 
 class DenseNet121(nn.Module):
     def __init__(self, densenet121_path):
         super().__init__()
-        densenet121_dict = torch.load(densenet121_path, weights_only=True)
+        densenet121_dict = torch.load(densenet121_path, weights_only=False)
         base_model = models.densenet121(weights=None)
         base_model.load_state_dict(densenet121_dict, strict=False)
         self.features = base_model.features
         self.pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Linear(1024, 1)
+        self.classifier = BayesianLinear(1024, 1)
 
     def forward(self, x):
         feature_map = self.features(x)
@@ -25,6 +25,6 @@ if __name__ == "__main__":
     model = DenseNet121("./resources/RadImageNet_pytorch/DenseNet121.pt")
 
     output = model(image)
-    print(f"Model: {summary(model)}")
+    print(f"Model: {(model)}")
     print(f"Input: {image.shape=}")
     print(f"Output: {output.shape=}")
